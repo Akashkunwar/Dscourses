3456f38aa2603f5f5ec0f4dd189dde8d45885f0d
Akash Kumar <gnspda@gmail.com>
2022-08-19 06:29:11 +0530

    Create 1 Main.java

---
+++ java/1 Main.java
@@ -0,0 +1,8 @@
+// Simple Hello world print
+public class Main {
+   public static void main(String[] args) {
+      System.out.println("Hello, World!");
+   }   
+}
+
+// rename file by removing number
