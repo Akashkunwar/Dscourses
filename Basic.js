91a7f235c9c76a5d2994ee0fde9a13df449bdd35
Akash Kumar <gnspda@gmail.com>
2023-02-01 08:02:48 +0530

    Add files via upload

---
+++ Basic.js
@@ -0,0 +1,30 @@
+// Assigning variable
+var a = -1; 
+console.log(a);
+
+// Manupulating variable
+b = 12
+a = b
+console.log(a);
+
+// Can't reasign const
+const c = 2
+console.log(c);
+// can't do c = 4
+
+let a = 23
+console.log(a)
+
+// // Types of variable
+// Number variable
+var a = 34
+console.log(a);
+
+// Bolliean variable
+var a = true
+console.log(a);
+
+// Strings
+var a = 'world';
+var b = `Hello ${a}`
+console.log(b);
