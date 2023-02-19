6cb6af01764c1ec61a1dba07be54ed56d2ab4dd2
Akash Kumar <gnspda@gmail.com>
2023-02-19 17:00:35 +0530

    Update Basic.js

--- JavaScript/Basic.js
+++ JavaScript/Basic.js
@@ -76,6 +76,55 @@ console.log("This is example of average operator : ", averag(45,46,34,65))
 // Importing functios
 // Math module (We don't need to import math module because it is pre installed)
 // Getting random values
-console.log("Give random numebr from inbuilt function : ",Math.random())
+console.log("Give random numebr from inbuilt function : ", Math.random())
 // Getting floor value of any number 
-console.log("Give random numebr from inbuilt function : ",Math.floor(32.234))
+console.log("Give random numebr from inbuilt function : ", Math.floor(32.234))
+
+// Conditionals
+// Equal to ===
+// Not equal to !==
+
+console.log('\nConditionals : ')
+function same(a,b) {
+    if(a===b) {
+        console.log("Variable", a, "and", b, "are same")
+    }
+    else {
+        console.log("Variables",a,'and',b, "are not same")
+    }
+}
+
+same(2,2)
+same(2,4)
+
+// Else can be skipptd
+console.log('\nElse can be skipped : ')
+function notsame(a,b) {
+    if(a!==b) {
+        return true
+    }
+    return false
+}
+console.log(notsame(3,3))
+console.log(notsame(3,2))
+
+console.log('\nWhat to eat with multiple conditions using else if')
+// Else if for multiple cases
+function whateat(a) {
+    if(a==='hot'){
+        console.log("In",a,"Eat Icecream")
+    }
+    else if(a==='cold'){
+        console.log("In",a,"Drink hot soup")
+    }
+    else if(a==='rain'){
+        console.log("In",a,"tea and snacks")
+    }
+    else{
+        console.log("In",a,"Eat anythink you like")
+    }
+}
+whateat('hot')
+whateat('cold')
+whateat('rain')
+whateat('spring')
